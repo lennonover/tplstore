4a3a14ccc803a8d2a06e0939bc9644a4d20d099e
lennonover <lennonover@163.com>
2018-07-10 16:52:48 +0800

    [updata] find()

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/login', function(req, res, next) {
-  
   var username = req.body.username;
   var password = req.body.password;
   console.log(username)
@@ -19,7 +18,7 @@ router.post('/login', function(req, res, next) {
       password: String
   });
   var User = db.model('goods',Schema);
-  User.find({}, function (err, data) {
+  User.findOne({username:username}, function (err, data) {
     console.log(err,data)
       if(!data) {
         res.json({
@@ -29,8 +28,7 @@ router.post('/login', function(req, res, next) {
         return;
       }
       if(data.password === password){
-          req.session.user = {username: username};
-          res.redirect('/');
+        req.session && (req.session.user = {username: username}); 
           res.json({
             code:200,
             token:5848484848
@@ -39,7 +37,7 @@ router.post('/login', function(req, res, next) {
       }
       res.json({
         code:404,
-        error:"用户名或密码不正确"
+        error:"密码不正确"
       }) 
   })
   
